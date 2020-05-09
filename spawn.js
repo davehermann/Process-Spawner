5df3e0020a1c6d4efb8ffb0dfbb06dada8b5582b
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-05-09 09:29:05 -0400

    Allow passing the process through for capturing stdout, stdin, and stderr

--- spawn.js
+++ spawn.js
@@ -4,10 +4,13 @@ const { spawn } = require(`child_process`);
  * Spawn a child process
  * @param {(String|Array<String>)} launchString - the entire string to run the process
  * @param {Object} spawnOptions - passed to the NodeJS spawn method
- * @param {Object} [dataOptions={}] - options for this method (isBinary, isJson)
+ * @param {Object} [dataOptions={}] - options for this method
+ * @param {Boolean} [dataOptions.isBinary] - Format binary output
+ * @param {Boolean} [dataOptions.isJson] - Format JSON output
+ * @param {Boolean} [dataOptions.consolePassthrough] - send process back to caller. MUST HANDLE "close" event
  * @returns {Promise} Promise will resolve to the type of data [if] supplied, or reject with the error
  */
-function spawnChildProcess(launchString, spawnOptions, dataOptions = {}) {
+function spawnChildProcess(launchString, spawnOptions = undefined, dataOptions = {}) {
     return new Promise((resolve, reject) => {
         let content = !!dataOptions && dataOptions.isBinary ? [] : ``,
             errText = ``,
@@ -38,29 +41,33 @@ function spawnChildProcess(launchString, spawnOptions, dataOptions = {}) {
 
         let spawnedProcess = spawn(launching.shift(), launching, spawnOptions);
 
-        spawnedProcess.stdout.on(`data`, (data) => {
-            if (dataOptions.isBinary)
-                content.push(data);
-            else
-                content += data;
-        });
-
-        spawnedProcess.stderr.on(`data`, (data) => {
-            errText += data;
-        });
-
-        spawnedProcess.on(`close`, () => {
-            if (!!errText)
-                reject(errText);
-            else {
+        if (dataOptions.consolePassthrough) {
+            resolve(spawnedProcess);
+        } else {
+            spawnedProcess.stdout.on(`data`, (data) => {
                 if (dataOptions.isBinary)
-                    resolve(Buffer.concat(content));
-                else if (dataOptions.isJson)
-                    resolve(JSON.parse(content));
+                    content.push(data);
                 else
-                    resolve(content);
-            }
-        });
+                    content += data;
+            });
+
+            spawnedProcess.stderr.on(`data`, (data) => {
+                errText += data;
+            });
+
+            spawnedProcess.on(`close`, () => {
+                if (!!errText)
+                    reject(errText);
+                else {
+                    if (dataOptions.isBinary)
+                        resolve(Buffer.concat(content));
+                    else if (dataOptions.isJson)
+                        resolve(JSON.parse(content));
+                    else
+                        resolve(content);
+                }
+            });
+        }
     });
 }
 
